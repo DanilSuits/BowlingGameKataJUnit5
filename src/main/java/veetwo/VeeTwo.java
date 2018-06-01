1e98b29440dff1699812761ba5bb432623968d8a
Danil <danil@vast.com>
2018-05-31 23:07:45 -0400

    GREEN: Remove Duplication

--- src/main/java/veetwo/VeeTwo.java
+++ src/main/java/veetwo/VeeTwo.java
@@ -11,7 +11,7 @@ package veetwo;
 public class VeeTwo {
 
     public interface Game {
-        int score(int ... readings);
+        int score(int... readings);
     }
 
     public Game game() {
@@ -19,22 +19,40 @@ public class VeeTwo {
 
             @Override
             public int score(int... readings) {
-                int [] pinsKnockedDown = pinsKnockedDown(readings);
+                int[] pinsKnockedDown = pinsKnockedDown(readings);
                 return veeOneScore(pinsKnockedDown);
             }
 
             private int veeOneScore(int[] pinsKnockedDown) {
-                // TODO:
-                return 300;
+                // TODO
+                int[] framePositions = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+                int gameScore = 0;
+                for (int firstBall : framePositions) {
+                    int secondBall = firstBall + 1;
+                    int thirdBall = secondBall + 1;
+
+                    // TODO
+                    int scoreInFrame
+                            = pinsKnockedDown[firstBall] +
+                            pinsKnockedDown[secondBall] +
+                            pinsKnockedDown[thirdBall];
+
+                    gameScore += scoreInFrame;
+                }
+                return gameScore;
             }
 
             private int[] pinsKnockedDown(int[] readings) {
-                // TODO
-                int [] perfectGame = {10,10,10,10,10,10,10,10,10,10};
+                int[] perfectGame = new int[readings.length];
+                for (int pos = 0; pos < perfectGame.length; ++pos) {
+                    // TODO
+                    perfectGame[pos] = 10 - readings[pos];
+                }
                 return perfectGame;
             }
         }
-        
+
         Game game = new SimpleGame();
 
         return game;
