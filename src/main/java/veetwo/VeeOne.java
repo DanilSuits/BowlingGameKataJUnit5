1d0b2e29a51e41b82b75d2968753e6b511365022
Danil <danil@vast.com>
2018-06-01 09:55:07 -0400

    GREEN: extract class

---
+++ src/main/java/veetwo/VeeOne.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright Vast 2018. All Rights Reserved.
+ * <p/>
+ * http://www.vast.com
+ */
+package veetwo;
+
+/**
+ * @author Danil Suits (danil@vast.com)
+ */
+class VeeOne {
+    interface Game {
+        int score(int... pinsKnocedDown);
+    }
+
+    public Game game() {
+        class VeeOneGame implements Game {
+            @Override
+            public int score(int... pinsKnockedDown) {
+                return veeOneScore(pinsKnockedDown);
+            }
+
+            private int veeOneScore(int[] pinsKnockedDown) {
+                int[] framePositions = framePositions(pinsKnockedDown);
+
+                int gameScore = 0;
+                for (int firstBall : framePositions) {
+                    int secondBall = firstBall + 1;
+                    int thirdBall = secondBall + 1;
+
+                    int scoreInFrame
+                            = pinsKnockedDown[firstBall] +
+                            pinsKnockedDown[secondBall];
+
+                    if (scoreInFrame >= 10) {
+                        scoreInFrame +=
+                                pinsKnockedDown[thirdBall];
+                    }
+
+                    gameScore += scoreInFrame;
+                }
+                return gameScore;
+            }
+
+            private int[] framePositions(int[] pinsKnockedDown) {
+                int[] framePositions = new int[10];
+                int nextFrame = 0;
+
+                for (int pos = 0; pos < framePositions.length; ++pos) {
+                    framePositions[pos] = nextFrame;
+                    if (10 == pinsKnockedDown[nextFrame]) {
+                        nextFrame += 1;
+                    } else {
+                        nextFrame += 2;
+                    }
+                }
+                return framePositions;
+            }
+
+        }
+
+        return new VeeOneGame();
+    }
+}
--- src/main/java/veetwo/VeeTwo.java
+++ src/main/java/veetwo/VeeTwo.java
@@ -9,57 +9,15 @@ package veetwo;
  * @author Danil Suits (danil@vast.com)
  */
 public class VeeTwo {
+    final VeeOne veeOne = new VeeOne();
 
     public interface Game {
         int score(int... readings);
     }
 
     public Game game() {
-        class VeeOneGame {
-            public int score(int... pinsKnockedDown) {
-                return veeOneScore(pinsKnockedDown);
-            }
-
-            private int veeOneScore(int[] pinsKnockedDown) {
-                int[] framePositions = framePositions(pinsKnockedDown);
-
-                int gameScore = 0;
-                for (int firstBall : framePositions) {
-                    int secondBall = firstBall + 1;
-                    int thirdBall = secondBall + 1;
-
-                    int scoreInFrame
-                            = pinsKnockedDown[firstBall] +
-                            pinsKnockedDown[secondBall];
-
-                    if (scoreInFrame >= 10) {
-                        scoreInFrame +=
-                                pinsKnockedDown[thirdBall];
-                    }
-
-                    gameScore += scoreInFrame;
-                }
-                return gameScore;
-            }
-
-            private int[] framePositions(int[] pinsKnockedDown) {
-                int[] framePositions = new int[10];
-                int nextFrame = 0;
-
-                for (int pos = 0; pos < framePositions.length; ++pos) {
-                    framePositions[pos] = nextFrame;
-                    if (10 == pinsKnockedDown[nextFrame]) {
-                        nextFrame += 1;
-                    } else {
-                        nextFrame += 2;
-                    }
-                }
-                return framePositions;
-            }
-
-        }
         class SimpleGame implements Game {
-            VeeOneGame veeOneGame = new VeeOneGame();
+            VeeOne.Game veeOneGame = veeOne.game();
 
             @Override
             public int score(int... readings) {
