454ba365010e4760f64a43b51efee163cc9c003b
Danil <danil@vast.com>
2018-06-01 09:46:10 -0400

    GREEN: Note 0012

--- src/main/java/veetwo/VeeTwo.java
+++ src/main/java/veetwo/VeeTwo.java
@@ -15,11 +15,58 @@ public class VeeTwo {
     }
 
     public Game game() {
+        class VeeOneGame {
+            public int score(int... pinsKnockedDown) {
+                return veeOneScore(pinsKnockedDown);
+            }
+
+            private int veeOneScore(int[] pinsKnockedDown) {
+                int[] framePositions = framePositions(pinsKnockedDown);
+
+                int gameScore = 0;
+                for (int firstBall : framePositions) {
+                    int secondBall = firstBall + 1;
+                    int thirdBall = secondBall + 1;
+
+                    int scoreInFrame
+                            = pinsKnockedDown[firstBall] +
+                            pinsKnockedDown[secondBall];
+
+                    if (scoreInFrame >= 10) {
+                        scoreInFrame +=
+                                pinsKnockedDown[thirdBall];
+                    }
+
+                    gameScore += scoreInFrame;
+                }
+                return gameScore;
+            }
+
+            private int[] framePositions(int[] pinsKnockedDown) {
+                int[] framePositions = new int[10];
+                int nextFrame = 0;
+
+                for (int pos = 0; pos < framePositions.length; ++pos) {
+                    framePositions[pos] = nextFrame;
+                    if (10 == pinsKnockedDown[nextFrame]) {
+                        nextFrame += 1;
+                    } else {
+                        nextFrame += 2;
+                    }
+                }
+                return framePositions;
+            }
+
+        }
         class SimpleGame implements Game {
+            VeeOneGame veeOneGame = new VeeOneGame();
 
             @Override
             public int score(int... readings) {
                 int[] pinsKnockedDown = pinsKnockedDown(readings);
+
+                if (true) return veeOneGame.score(pinsKnockedDown);
+
                 return veeOneScore(pinsKnockedDown);
             }
 
